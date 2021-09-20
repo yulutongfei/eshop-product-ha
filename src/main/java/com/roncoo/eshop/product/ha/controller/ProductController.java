f70388390fd9840d7ca87e92673c458fde7cd63f
孙许 <2572012446@qq.com>
2021-09-20 14:40:43 +0800

    hystrix_learning

---
+++ src/main/java/com/roncoo/eshop/product/ha/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.roncoo.eshop.product.ha.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author 孙许
+ * @version 1.0
+ * @date 2021/9/19 17:47
+ */
+@Controller
+public class ProductController {
+
+    @RequestMapping("/getProductInfo")
+    @ResponseBody
+    public String getProductInfo(Long productId) {
+        return  "{\n" +
+                "  \"id\": " + productId + ",\n" +
+                "  \"name\": \"iphone7手机\",\n" +
+                "  \"price\": 5599,\n" +
+                "  \"pictureList\": \"a.jpg,b.jpg\",\n" +
+                "  \"specification\": \"iphone7的规格\",\n" +
+                "  \"service\": \"iphone7的售后服务\",\n" +
+                "  \"color\": \"红色,白色,黑色\",\n" +
+                "  \"size\": 5.5,\n" +
+                "  \"shopId\": 1,\n" +
+                "  \"modifiedTime\": \"2021-06-28 12:01:00\"\n" +
+                "}";
+    }
+}
